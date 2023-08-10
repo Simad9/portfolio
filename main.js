b74882948b9836d13cc776e337386d724d33fe6d
Simad9 <wijdanbita@gmail.com>
2023-08-10 23:04:20 +0700

    sampe about

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('oke');
+// Menu burger dan menu x
+const menu = document.querySelector('.menu ');
+menu.addEventListener('click', () => {
+  let img = document.querySelector('.menu img').src;
+  if (img.indexOf('burger') == -1) {
+    document.querySelector('.menu img').src = 'img/burger.png';
+  } else {
+    document.querySelector('.menu img').src = 'img/x.png';
+  }
+})
