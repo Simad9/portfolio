f74fd026b30d8f6bf728dd352c995ce2f38d10c6
Simad9 <wijdanbita@gmail.com>
2023-08-12 09:38:48 +0700

    tambahan baru

--- main.js
+++ main.js
@@ -6,11 +6,32 @@ menuIcon.addEventListener('click', () => {
   navigation.classList.toggle('active')
 })
 
-// Saat milih langsung nutup
+// scroll active
+const sections = document.querySelectorAll('section')
+const navLinks = document.querySelectorAll('header .navigation .nav-item')
+let id = 'about'
 window.addEventListener('scroll', () => {
+  sections.forEach(sec => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 200
+    let height = sec.offsetHeight
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header .navigation a[href="#' + id + '"').parentNode.classList.add('active')
+      })
+
+    }
+  })
   menuIcon.classList.remove('bx-x')
   navigation.classList.remove('active')
 })
+
+// Scroll offset
+const navigationHeight = document.querySelector('.navigation').offsetHeight
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 5 + "px")
 
 // simulasi kirim pesan
 const btnSubmit = document.querySelector('input[type=\'submit\'] ');
@@ -18,3 +39,8 @@ btnSubmit.addEventListener('click', () => {
   alert('Ceritanya pesan terkirim')
 })
 
+// lihat all project
+const btnProj = document.querySelector('.projects .btn')
+btnProj.addEventListener('click', () => {
+  confirm('Halaman belum dibuat, project lain ada di portfolio. Apakah anda ingin membukanya?')
+})
